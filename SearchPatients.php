d01147ffd85b1f4e2f41a8732d0691c465fde09a
Tim Schofield <tim@weberpafrica.com>
2015-02-13 16:18:06 +0000

    New patient search functionality

---
+++ SearchPatients.php
@@ -0,0 +1,108 @@
+<?php
+$PageSecurity = 1;
+require_once('includes/session.inc');
+
+$SearchAnswers = array();
+$i = 0;
+/* First check for an exact match */
+$SQL = "SELECT pid,
+				name_first,
+				name_middle,
+				name_last,
+				phone_1_nr,
+				sex,
+				date_birth
+			FROM care_person
+			WHERE name_first='" . $_POST['FirstName'] . "'
+				AND name_middle='" . $_POST['OtherName'] . "'
+				AND name_last='" . $_POST['LastName'] . "'
+				AND phone_1_nr='" . $_POST['Telephone'] . "'
+				AND sex='" . $_POST['Sex'] . "'";
+$Result = DB_query($SQL);
+$TotalRows = DB_num_rows($Result);
+while ($MyRow = DB_fetch_array($Result)) {
+	$SearchAnswers[$i]['PID'] = $MyRow['pid'];
+	$SearchAnswers[$i]['FirstName'] = $MyRow['name_first'];
+	$SearchAnswers[$i]['MiddleName'] = $MyRow['name_middle'];
+	$SearchAnswers[$i]['LastName'] = $MyRow['name_last'];
+	$SearchAnswers[$i]['Telephone'] = $MyRow['phone_1_nr'];
+	if ($MyRow['sex'] == 'm' ) {
+		$SearchAnswers[$i]['Gender'] = _('Male');
+	} else {
+		$SearchAnswers[$i]['Gender'] = _('Female');
+	}
+	$SearchAnswers[$i]['DOB'] = $MyRow['date_birth'];
+	++$i;
+}
+
+/* If no exact match then check for a matching telephone number */
+if ($TotalRows == 0 and $_POST['Telephone'] != '') {
+	$SQL = "SELECT pid,
+					name_first,
+					name_middle,
+					name_last,
+					phone_1_nr,
+					sex,
+					date_birth
+				FROM care_person
+				WHERE REPLACE(phone_1_nr, ' ', '')='" . str_replace(' ', '', $_POST['Telephone']) . "'";
+	$Result = DB_query($SQL);
+	while ($MyRow = DB_fetch_array($Result)) {
+		$SearchAnswers[$i]['PID'] = $MyRow['pid'];
+		$SearchAnswers[$i]['FirstName'] = $MyRow['name_first'];
+		$SearchAnswers[$i]['MiddleName'] = $MyRow['name_middle'];
+		$SearchAnswers[$i]['LastName'] = $MyRow['name_last'];
+		$SearchAnswers[$i]['Telephone'] = $MyRow['phone_1_nr'];
+		if ($MyRow['sex'] == 'm' ) {
+			$SearchAnswers[$i]['Gender'] = _('Male');
+		} else {
+			$SearchAnswers[$i]['Gender'] = _('Female');
+		}
+		$SearchAnswers[$i]['DOB'] = $MyRow['date_birth'];
+		++$i;
+	}
+}
+
+/* If no exact match then check for a nearest sounding match */
+if ($TotalRows == 0) {
+	$SQL = "SELECT pid,
+					name_first,
+					name_middle,
+					name_last,
+					phone_1_nr,
+					sex,
+					date_birth
+				FROM care_person
+				WHERE CONCAT(name_first,' ',name_last) SOUNDS LIKE '" . $_POST['FirstName'] . ' ' . $_POST['LastName'] . "'";
+	$Result = DB_query($SQL);
+	while ($MyRow = DB_fetch_array($Result)) {
+		$SearchAnswers[$i]['PID'] = $MyRow['pid'];
+		$SearchAnswers[$i]['FirstName'] = $MyRow['name_first'];
+		$SearchAnswers[$i]['MiddleName'] = $MyRow['name_middle'];
+		$SearchAnswers[$i]['LastName'] = $MyRow['name_last'];
+		$SearchAnswers[$i]['Telephone'] = $MyRow['phone_1_nr'];
+		if ($MyRow['sex'] == 'm' ) {
+			$SearchAnswers[$i]['Gender'] = _('Male');
+		} else {
+			$SearchAnswers[$i]['Gender'] = _('Female');
+		}
+		$SearchAnswers[$i]['DOB'] = $MyRow['date_birth'];
+		++$i;
+	}
+}
+
+foreach ($SearchAnswers as $Answer) {
+	echo '<input type="hidden" id="fst' . $Answer['PID'] . '" value="' . $Answer['FirstName'] . '" />';
+	echo '<input type="hidden" id="lst' . $Answer['PID'] . '" value="' . $Answer['LastName'] . '" />';
+	echo '<input type="hidden" id="mid' . $Answer['PID'] . '" value="' . $Answer['MiddleName'] . '" />';
+	echo '<tr>
+			<td><input onclick="FillForm(this)" type="radio" name="Patient" value="' . $Answer['PID'] . '" /></td>
+			<td>' . $Answer['PID'] . '</td>
+			<td>' . $Answer['FirstName'] . ' ' . $Answer['LastName'] . '</td>
+			<td id="Tel' . $Answer['PID'] . '">' . $Answer['Telephone'] . '</td>
+			<td id="Sex' . $Answer['PID'] . '">' . $Answer['Gender'] . '</td>
+			<td id="Dob' . $Answer['PID'] . '">' . ConvertSQLDate($Answer['DOB']) . '</td>
+		</tr>';
+}
+
+?>
---
+++ sql/updates/119.php
@@ -0,0 +1,78 @@
+<?php
+
+CreateTable('care_address1',
+"CREATE TABLE care_address1 (
+  `country` CHAR(2) NOT NULL DEFAULT '',
+  `address_code` INT(11) NOT NULL DEFAULT 0,
+  `address_name` VARCHAR(100) NOT NULL DEFAULT '',
+  `form_label` VARCHAR(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`country`, `address_code`),
+  KEY (`address_code`)
+)");
+
+CreateTable('care_address2',
+"CREATE TABLE care_address2 (
+  `country` CHAR(2) NOT NULL DEFAULT '',
+  `address1` INT(11) NOT NULL DEFAULT 0,
+  `address_code` INT(11) NOT NULL DEFAULT 0,
+  `address_name` VARCHAR(100) NOT NULL DEFAULT '',
+  `form_label` VARCHAR(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`country`, `address_code`),
+  KEY (`address_code`),
+  CONSTRAINT `careaddr2_ibfk_1` FOREIGN KEY (`address1`) REFERENCES `care_address1` (`address_code`)
+)");
+
+CreateTable('care_address3',
+"CREATE TABLE care_address3 (
+  `country` CHAR(2) NOT NULL DEFAULT '',
+  `address2` INT(11) NOT NULL DEFAULT 0,
+  `address_code` INT(11) NOT NULL DEFAULT 0,
+  `address_name` VARCHAR(100) NOT NULL DEFAULT '',
+  `form_label` VARCHAR(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`country`, `address_code`),
+  KEY (`address_code`),
+  CONSTRAINT `careaddr3_ibfk_1` FOREIGN KEY (`address2`) REFERENCES `care_address2` (`address_code`)
+)");
+
+CreateTable('care_address4',
+"CREATE TABLE care_address4 (
+  `country` CHAR(2) NOT NULL DEFAULT '',
+  `address3` INT(11) NOT NULL DEFAULT 0,
+  `address_code` INT(11) NOT NULL DEFAULT 0,
+  `address_name` VARCHAR(100) NOT NULL DEFAULT '',
+  `form_label` VARCHAR(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`country`, `address_code`),
+  KEY (`address_code`),
+  CONSTRAINT `careaddr4_ibfk_1` FOREIGN KEY (`address3`) REFERENCES `care_address3` (`address_code`)
+)");
+
+CreateTable('care_address5',
+"CREATE TABLE care_address5 (
+  `country` CHAR(2) NOT NULL DEFAULT '',
+  `address4` INT(11) NOT NULL DEFAULT 0,
+  `address_code` INT(11) NOT NULL DEFAULT 0,
+  `address_name` VARCHAR(100) NOT NULL DEFAULT '',
+  `form_label` VARCHAR(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`country`, `address_code`),
+  KEY (`address_code`),
+  CONSTRAINT `careaddr5_ibfk_1` FOREIGN KEY (`address4`) REFERENCES `care_address4` (`address_code`)
+)");
+
+executeSQL("TRUNCATE care_address1");
+executeSQL("INSERT INTO care_address1 SELECT 'UG', id, district_name, '" . _('District') . "' FROM care_ug_districts");
+
+executeSQL("TRUNCATE care_address2");
+executeSQL("INSERT INTO care_address2 SELECT 'UG', district_id, id, county, '" . _('County') . "' FROM care_ug_county");
+
+executeSQL("TRUNCATE care_address3");
+executeSQL("INSERT INTO care_address3 SELECT 'UG', county_id, id, subcounty, '" . _('Sub-County') . "' FROM care_ug_subcounty");
+
+executeSQL("TRUNCATE care_address4");
+executeSQL("INSERT INTO care_address4 SELECT 'UG', subcounty_id, id, parish, '" . _('Parish') . "' FROM care_ug_parish");
+
+executeSQL("TRUNCATE care_address5");
+executeSQL("INSERT INTO care_address5 SELECT 'UG', parish_id, id, village, '" . _('Village') . "' FROM care_ug_village");
+
+UpdateDBNo(basename(__FILE__, '.php'));
+
+?>
