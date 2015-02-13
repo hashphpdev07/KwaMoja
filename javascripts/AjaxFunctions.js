3cf2037690bb832a60631aa54bc70ca7cd3b562e
Tim Schofield <tim@weberpafrica.com>
2015-02-13 16:15:08 +0000

    Include the ajax functions

--- javascripts/AjaxFunctions.js
+++ javascripts/AjaxFunctions.js
@@ -24,3 +24,31 @@ function SubmitForm(FormName, Element) {
 	xmlhttp.send(PostData);
 	return false;
 }
+
+
+function SearchPatients(FormName) {
+	Target='SearchPatients.php';
+	var PostData='';
+	for(var i=0,fLen=FormName.length;i<fLen;i++){
+		if(FormName.elements[i].type=='checkbox' && !FormName.elements[i].checked) {
+			FormName.elements[i].value=null;
+		}
+		PostData=PostData+FormName.elements[i].name+'='+FormName.elements[i].value+'&';
+	}
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	} else {// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			document.getElementById('SearchResults').innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("POST",Target,true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.setRequestHeader("Cache-Control","no-store, no-cache, must-revalidate");
+	xmlhttp.setRequestHeader("Pragma","no-cache");
+	xmlhttp.send(PostData);
+	return false;
+}
