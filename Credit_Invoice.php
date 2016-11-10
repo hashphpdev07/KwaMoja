f41049b18a3f51911408496ce34abdd1c4a4a88c
Tim Schofield <tim@weberpafrica.com>
2016-11-10 10:37:09 +0000

    Fix bug in boolean operator

--- Credit_Invoice.php
+++ Credit_Invoice.php
@@ -213,7 +213,7 @@ if (isset($_POST['Location'])) {
 	}
 }
 
-if (isset($_POST['ChargeFreightCost']) and $_POST['ChargeFreightCost'] !== 0) {
+if (isset($_POST['ChargeFreightCost']) and $_POST['ChargeFreightCost'] != 0) {
 	$_SESSION['CreditItems' . $Identifier]->FreightCost = filter_number_format($_POST['ChargeFreightCost']);
 	if (($TotalQtyCredited + abs($_POST['ChargeFreightCost'])) <= 0) {
 		prnMsg(_('There are no item quantity or freight charge input'), 'error');
