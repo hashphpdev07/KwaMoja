010bc4c28b8904e98513a2f862378a471df31338
Phil Daintree <phil@logicworks.co.nz>
2017-07-15 09:39:53 +0100

    Take out duplicated query

--- ReorderLevel.php
+++ ReorderLevel.php
@@ -107,17 +107,6 @@ if (isset($_POST['PrintPDF'])) {
 		if ($YPos < $Bottom_Margin + $line_height) {
 			PrintHeader($PDF, $YPos, $PageNumber, $Page_Height, $Top_Margin, $Left_Margin, $Page_Width, $Right_Margin, $CategoryDescription);
 		}
-		$OnOrderSQL = "SELECT SUM(quantityord-quantityrecd) AS quantityonorder
-								FROM purchorders
-								LEFT JOIN purchorderdetails
-								ON purchorders.orderno=purchorderdetails.orderno
-								WHERE purchorders.status != 'Cancelled'
-									AND purchorders.status != 'Rejected'
-									AND purchorders.status != 'Pending'
-									  AND purchorderdetails.itemcode='" . $MyRow['stockid'] . "'
-									  AND purchorders.intostocklocation='" . $MyRow['loccode'] . "'";
-		$OnOrderResult = DB_query($OnOrderSQL);
-		$OnOrderRow = DB_fetch_array($OnOrderResult);
 
 		// Print if stock for part in other locations
 		$SQL2 = "SELECT locstock.quantity,
