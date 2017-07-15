9c347a88fecd2e58bbcaa58829ebc9b912ee29c3
Tim Schofield <tim@weberpafrica.com>
2017-07-15 09:55:41 +0100

    Improve the look of screen

--- Z_RemovePurchaseBackOrders.php
+++ Z_RemovePurchaseBackOrders.php
@@ -9,8 +9,10 @@ include('includes/header.php');
 echo '<form method="post" action="', htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8'), '">';
 echo '<input type="hidden" name="FormID" value="', $_SESSION['FormID'], '" />';
 
-echo '<div class="centre">',
-		_('This will alter all purchase orders where the quantity required is more than the quantity delivered - where some has been delivered already. The quantity ordered will be reduced to the same as the quantity already delivered - removing all back orders'), '<br />
+echo '<br /><div class="page_help_text">',
+		_('This will alter all purchase orders where the quantity required is more than the quantity delivered - where some has been delivered already. The quantity ordered will be reduced to the same as the quantity already delivered - removing all back orders'), '
+	</div>
+	<div class="centre">
 		<input type="submit" name="RemovePOBackOrders" value="', _('Remove Purchase Back Orders'), '" />
 	</div>
 </form>';
