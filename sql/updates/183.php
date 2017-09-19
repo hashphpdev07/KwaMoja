f0c1bf8dcc85e7786fc00950eedcfd4c89f52c88
Tim Schofield <tim@weberpafrica.com>
2017-09-19 10:50:43 +0100

    Database updates for supplier groups

---
+++ sql/updates/183.php
@@ -0,0 +1,24 @@
+<?php
+
+CreateTable('suppliergroups',
+"CREATE TABLE `suppliergroups` (
+  `id` int(11) NOT NULL auto_increment,
+  `coyname` varchar(50) NOT NULL default '',
+  `address1` varchar(40) NOT NULL default '',
+  `address2` varchar(40) NOT NULL default '',
+  `address3` varchar(40) NOT NULL default '',
+  `address4` varchar(40) NOT NULL default '',
+  `address5` varchar(20) NOT NULL default '',
+  `address6` varchar(15) NOT NULL default '',
+  `contact` varchar(25) NOT NULL default '',
+  `telephone` varchar(25) NOT NULL default '',
+  `fax` varchar(25) NOT NULL default '',
+  `email` varchar(55) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+)");
+
+AddColumn('suppliergroupid', 'suppliers', 'INT(11)', 'NOT NULL', '0', 'factorcompanyid');
+
+UpdateDBNo(basename(__FILE__, '.php'));
+
+?>
