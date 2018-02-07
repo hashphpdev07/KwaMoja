aa0ab7db07bfd4a6a041acc5b0de866872851301
Tim Schofield <tim@weberpafrica.com>
2018-02-07 10:16:17 +0000

    Use new form layout css Use new reporting style, output to the browser first with an option to print the report then

--- BOMListing.php
+++ BOMListing.php
@@ -1,10 +1,9 @@
 <?php
-
-include('includes/session.php');
+include ('includes/session.php');
 
 if (isset($_POST['PrintPDF']) and isset($_POST['FromCriteria']) and mb_strlen($_POST['FromCriteria']) >= 1 and isset($_POST['ToCriteria']) and mb_strlen($_POST['ToCriteria']) >= 1) {
 
-	include('includes/PDFStarter.php');
+	include ('includes/PDFStarter.php');
 	$PDF->addInfo('Title', _('Bill Of Material Listing'));
 	$PDF->addInfo('Subject', _('Bill Of Material Listing'));
 	$FontSize = 12;
@@ -20,14 +19,14 @@ if (isset($_POST['PrintPDF']) and isset($_POST['FromCriteria']) and mb_strlen($_
 				bom.quantity,
 				bom.loccode,
 				bom.workcentreadded,
-				bom.effectiveto AS eff_to,
-				bom.effectiveafter AS eff_frm
+				bom.effectiveto,
+				bom.effectiveafter
 			FROM stockmaster
 			INNER JOIN bom
 				ON stockmaster.stockid=bom.component
 			INNER JOIN locationusers
 				ON locationusers.loccode=bom.loccode
-				AND locationusers.userid='" .  $_SESSION['UserID'] . "'
+				AND locationusers.userid='" . $_SESSION['UserID'] . "'
 				AND locationusers.canview=1
 			WHERE bom.parent >= '" . $_POST['FromCriteria'] . "'
 				AND bom.parent <= '" . $_POST['ToCriteria'] . "'
@@ -37,80 +36,77 @@ if (isset($_POST['PrintPDF']) and isset($_POST['FromCriteria']) and mb_strlen($_
 					bom.component";
 
 	$BOMResult = DB_query($SQL, '', '', false, false); //dont do error trapping inside DB_query
+	$Title = _('Bill Of Material Listing for Parts Between') . ' ' . $_POST['FromCriteria'] . ' ' . _('and') . ' ' . $_POST['ToCriteria'];
+	include ('includes/header.php');
 
-	if (DB_error_no() != 0) {
-		$Title = _('Bill of Materials Listing') . ' - ' . _('Problem Report');
-		include('includes/header.php');
-		prnMsg(_('The Bill of Material listing could not be retrieved by the SQL because'), 'error');
-		echo '<br /><a href="' . $RootPath . '/index.php">' . _('Back to the menu') . '</a>';
-		if ($Debug == 1) {
-			echo '<br />' . $SQL;
-		}
-		include('includes/footer.php');
-		exit;
-	}
-	if (DB_num_rows($BOMResult) == 0) {
-		$Title = _('Bill of Materials Listing') . ' - ' . _('Problem Report');
-		include('includes/header.php');
-		prnMsg(_('The Bill of Material listing has no bills to report on'), 'warn');
-		include('includes/footer.php');
-		exit;
-	}
-
-	include('includes/PDFBOMListingPageHeader.php');
+	echo '<table>
+			<thead>
+				<tr class="noPrint">
+					<th colspan="9"><h2>', $Title, '</h2>
+						<img src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/printer.png" class="PrintIcon" title="', _('Print this report'), '" alt="', _('Print'), '" onclick="window.print();" />
+					</th>
+				</tr>
+				<tr>
+					<td colspan="7"><h3>
+						', $_SESSION['CompanyRecord']['coyname'], '<br />
+						', $Title, '<br />
+					</td>
+					<td style="float:right;vertical-align:top;text-align:right">
+						', _('Printed On'), ' ', Date($_SESSION['DefaultDateFormat']) . '
+					</td></h3>
+				</tr>
+				<tr>
+					<th colspan="2">', _('Component/Part Description'), '</th>
+					<th>', _('Effective After'), '</th>
+					<th>', _('Effective To'), '</th>
+					<th>', _('Location'), '</th>
+					<th>', _('Work Centre'), '</th>
+					<th colspan="2">', _('Quantity'), '</th>
+				</tr>
+			</thead>
+			<tbody>';
 
 	$ParentPart = '';
-
-	while ($BOMList = DB_fetch_array($BOMResult)) {
-
-		if ($ParentPart != $BOMList['parent']) {
-
-			$FontSize = 10;
-			if ($ParentPart != '') {
-				/*Then it's NOT the first time round */
-				/* need to rule off from the previous parent listed */
-				$YPos -= $line_height;
-				$PDF->line($Page_Width - $Right_Margin, $YPos, $Left_Margin, $YPos);
-				$YPos -= $line_height;
-			}
-			$SQL = "SELECT description FROM stockmaster WHERE stockmaster.stockid = '" . $BOMList['parent'] . "'";
+	while ($MyRow = DB_fetch_array($BOMResult)) {
+		if ($ParentPart != $MyRow['parent']) {
+			$SQL = "SELECT description FROM stockmaster WHERE stockmaster.stockid = '" . $MyRow['parent'] . "'";
 			$ParentResult = DB_query($SQL);
-			$ParentRow = DB_fetch_row($ParentResult);
-			$LeftOvers = $PDF->addTextWrap($Left_Margin, $YPos, 400 - $Left_Margin, $FontSize, $BOMList['parent'] . ' - ' . $ParentRow[0], 'left');
-			$ParentPart = $BOMList['parent'];
+			$ParentRow = DB_fetch_array($ParentResult);
+			if ($ParentPart != '') {
+				echo '<tr>
+						<td colspan="8"><hr></hr></th>
+					</tr>';
+			}
+			echo '<tr>
+						<td colspan="8"><h4>', $MyRow['parent'], ' - ', $ParentRow['description'], '</h4></td>
+					</tr>';
+			$ParentPart = $MyRow['parent'];
 		}
 
-		$YPos -= $line_height;
-		$FontSize = 8;
-		$LeftOvers = $PDF->addTextWrap($Left_Margin + 5, $YPos, 80, $FontSize, $BOMList['component'], 'left');
-		$LeftOvers = $PDF->addTextWrap(110, $YPos, 200, $FontSize, $BOMList['compdescription'], 'left');
-
-		$DisplayQuantity = locale_number_format($BOMList['quantity'], $BOMList['decimalplaces']);
-		$LeftOvers = $PDF->addTextWrap(320, $YPos, 50, $FontSize, ConvertSQLDate($BOMList['eff_frm']), 'left');
-		$LeftOvers = $PDF->addTextWrap(375, $YPos, 50, $FontSize, ConvertSQLDate($BOMList['eff_to']), 'left');
-		$LeftOvers = $PDF->addTextWrap(430, $YPos, 30, $FontSize, $BOMList['loccode'], 'left');
-		$LeftOvers = $PDF->addTextWrap(465, $YPos, 30, $FontSize, $BOMList['workcentreadded'], 'left');
-		$LeftOvers = $PDF->addTextWrap(480, $YPos, 60, $FontSize, $DisplayQuantity, 'right');
-		$LeftOvers = $PDF->addTextWrap(540, $YPos, 20, $FontSize, $BOMList['units'], 'left');
-
-		if ($YPos < $Bottom_Margin + $line_height) {
-			include('includes/PDFBOMListingPageHeader.php');
-		}
-
+		echo '<tr>
+				<td>', $MyRow['component'], '</td>
+				<td>', $MyRow['compdescription'], '</td>
+				<td>', ConvertSQLDate($MyRow['effectiveafter']), '</td>
+				<td>', ConvertSQLDate($MyRow['effectiveto']), '</td>
+				<td>', $MyRow['loccode'], '</td>
+				<td>', $MyRow['workcentreadded'], '</td>
+				<td class="number">', locale_number_format($MyRow['quantity'], 'Variable'), '</td>
+				<td>', $MyRow['units'], '</td>
+			</tr>';
 	}
-	/*end BOM Listing while loop */
-
-	$YPos -= $line_height;
-	$PDF->line($Page_Width - $Right_Margin, $YPos, $Left_Margin, $YPos);
-
-	$PDF->OutputD($_SESSION['DatabaseName'] . '_BOMListing_' . date('Y-m-d') . '.pdf');
-	$PDF->__destruct();
+	echo '<tr>
+			<td colspan="8"><hr></hr></th>
+		</tr>';
+	echo '</tbody>
+		</table>';
+	echo '<a class="noPrint" href="', htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8'), '">', _('Select different criteria'), '</a><br />';
+	include ('includes/footer.php');
 
 } else {
 	/*The option to print PDF was not hit */
 
 	$Title = _('Bill Of Material Listing');
-	include('includes/header.php');
+	include ('includes/header.php');
 
 	$SQL = "SELECT min(stockid) AS fromcriteria,
 					max(stockid) AS tocriteria
@@ -119,32 +115,37 @@ if (isset($_POST['PrintPDF']) and isset($_POST['FromCriteria']) and mb_strlen($_
 	$Result = DB_query($SQL);
 	$MyRow = DB_fetch_array($Result);
 
-	echo '<p class="page_title_text" ><img src="' . $RootPath . '/css/' . $_SESSION['Theme'] . '/images/reports.png" title="' . _('Search') . '" alt="" />' . ' ' . $Title . '</p>';
-	if (!isset($_POST['FromCriteria']) or !isset($_POST['ToCriteria'])) {
+	echo '<p class="page_title_text">
+			<img src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/reports.png" title="', _('Search'), '" alt="" />', ' ', $Title, '
+		</p>';
 
+	if (!isset($_POST['FromCriteria']) or !isset($_POST['ToCriteria'])) {
 		/*if $FromCriteria is not set then show a form to allow input	*/
 
-		echo '<form action="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '" method="post">
-			  <input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />
-			  <table>';
+		echo '<form action="', htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8'), '" method="post">';
+		echo '<input type="hidden" name="FormID" value="', $_SESSION['FormID'], '" />';
 
-		echo '<tr>
-				<td>' . _('From Inventory Part Code') . ':' . '</td>
-				<td><input type="text" name="FromCriteria" size="20" autofocus="autofocus" required="required" maxlength="20" value="' . $MyRow['fromcriteria'] . '" /></td>
-			</tr>';
+		echo '<fieldset>
+				<legend>', _('Enter Report Criteria'), '</legend>
+				<field>
+					<label for="FromCriteria">', _('From Inventory Part Code'), ':</label>
+					<input type="text" name="FromCriteria" size="20" autofocus="autofocus" required="required" maxlength="20" value="', $MyRow['fromcriteria'], '" />
+					<fieldhelp>', _('Starting stock code'), '</fieldhelp>
+				</field>
+				<field>
+					<label for="ToCriteria">', _('To Inventory Part Code'), ':</label>
+					<input type="text" name="ToCriteria" size="20" required="required" maxlength="20" value="', $MyRow['tocriteria'], '" />
+					<fieldhelp>', _('Final stock code'), '</fieldhelp>
+				</field>
+			</fieldset>';
 
-		echo '<tr>
-				<td>' . _('To Inventory Part Code') . ':' . '</td>
-				<td><input type="text" name="ToCriteria" size="20" required="required" maxlength="20" value="' . $MyRow['tocriteria'] . '" /></td>
-			</tr>';
-
-
-		echo '</table>
-				<div class="centre"><input type="submit" name="PrintPDF" value="' . _('Print PDF') . '" /></div>
-			 </form>';
+		echo '<div class="centre">
+				<input type="submit" name="PrintPDF" value="', _('View Report'), '" />
+			</div>
+		</form>';
 
 	}
-	include('includes/footer.php');
+	include ('includes/footer.php');
 
 }
 /*end of else not PrintPDF */
