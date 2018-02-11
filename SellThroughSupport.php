0b37f7b0d4f623122eb89552af60c936785c54f0
Paul Thursby <pthursby2@gmail.com>
2018-02-11 22:10:14 +0000

    Remove (another) redundant hidden FormID input. (there were two, overlooked the 2nd one earlier)

--- SellThroughSupport.php
+++ SellThroughSupport.php
@@ -152,7 +152,6 @@ if (isset($_POST['SearchSupplier'])) {
 
 	echo '<form action="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '" method="post">
 			<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />
-			<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />
 			<table cellpadding="2" colspan="7">
 				<thead>
 					<tr>
