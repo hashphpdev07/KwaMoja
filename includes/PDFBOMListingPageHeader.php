3fc69e24702ada1f90525036c0bc0f39214137d0
Paul Thursby <pthursby2@gmail.com>
2018-02-11 22:06:57 +0000

    Remove redundant hidden FormID input.

--- includes/PDFBOMListingPageHeader.php
+++
@@ -1,4 +0,0 @@
-Exception: File '/home/tim/code/KwaMoja/includes/PDFBOMListingPageHeader.php' doesn't exists in /usr/share/php/PHP/Beautifier.php:608
-Stack trace:
-#0 /home/tim/code/KwaMoja/.git/hooks/pre-commit.d/bin/format.phps(20): PHP_Beautifier->setInputFile('/home/tim/code/...')
-#1 {main}
