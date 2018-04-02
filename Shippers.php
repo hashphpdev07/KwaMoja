60d5d56063ef4de51676e3bf4df125d9d231cdf7
Tim Schofield <tim@weberpafrica.com>
2018-04-02 10:23:32 +0100

    Use new forms css and clean up the code
    Files changed in commit:
    Shippers.php
    On branch develop
    
    Make use of mincharge field previously in the table but not used.
    Make selected columns sortable
    Use echo for the data table rather than printf for better
    readability and consistency with the rest of the code
    base
    Use new css layout for forms for nicer looking input form

--- Shippers.php
+++ Shippers.php
@@ -34,14 +34,20 @@ if (isset($_POST['submit'])) {
 		would not run in this case cos submit is false of course  see the
 		delete code below*/
 
-		$SQL = "UPDATE shippers SET shippername='" . $_POST['ShipperName'] . "'
+		$SQL = "UPDATE shippers SET shippername='" . $_POST['ShipperName'] . "',
+									mincharge='" . filter_number_format($_POST['MinimumCharge']) . "'
 				WHERE shipper_id = '" . $SelectedShipper . "'";
 		$Msg = _('The shipper record has been updated');
 	} elseif ($InputError != 1) {
 
 		/*SelectedShipper is null cos no item selected on first time round so must be adding a	record must be submitting new entries in the new Shipper form */
 
-		$SQL = "INSERT INTO shippers (shippername) VALUES ('" . $_POST['ShipperName'] . "')";
+		$SQL = "INSERT INTO shippers (shippername,
+									  mincharge
+									 ) VALUES (
+										'" . $_POST['ShipperName'] . "',
+										'" . filter_number_format($_POST['MinimumCharge']) . "'
+									)";
 		$Msg = _('The shipper record has been added');
 	}
 
@@ -103,77 +109,106 @@ if (!isset($SelectedShipper)) {
 	links to delete or edit each. These will call the same page again and allow update/input
 	or deletion of the records*/
 	echo '<p class="page_title_text" >
-			<img src="' . $RootPath . '/css/' . $_SESSION['Theme'] . '/images/supplier.png" title="' . _('Search') . '" alt="" />' . ' ' . $Title . '</p>';
+			<img src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/supplier.png" title="', _('Search'), '" alt="" />', ' ', $Title, '
+		</p>';
 
-	$SQL = "SELECT * FROM shippers ORDER BY shipper_id";
+	$SQL = "SELECT shipper_id,
+					shippername,
+					mincharge
+				FROM shippers
+				ORDER BY shipper_id";
 	$Result = DB_query($SQL);
 
 	echo '<table>
-			<tr>
-				<th>' . _('Shipper ID') . '</th>
-				<th>' . _('Shipper Name') . '</th>
-			</tr>';
+			<thead>
+				<tr>
+					<th class="SortedColumn">', _('Shipper ID'), '</th>
+					<th class="SortedColumn">', _('Shipper Name'), '</th>
+					<th>', _('Minimum charge'), '</th>
+					<th colspan="2"></th>
+				</tr>
+			</thead>';
+	echo '<tbody>';
 
 	while ($MyRow = DB_fetch_array($Result)) {
-		printf('<tr class="striped_row">
-					<td>%s</td>
-					<td>%s</td>
-					<td><a href="%sSelectedShipper=%s">' . _('Edit') . '</a></td>
-					<td><a href="%sSelectedShipper=%s&amp;delete=1" onclick="return MakeConfirm(\'' . _('Are you sure you wish to delete this shipper?') . '\', \'Confirm Delete\', this);">' . _('Delete') . '</a></td>
-				</tr>', $MyRow[0], $MyRow[1], htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8') . '?', $MyRow[0], htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8') . '?', $MyRow[0]);
+		echo '<tr class="striped_row">
+				<td>', $MyRow['shipper_id'], '</td>
+				<td>', $MyRow['shippername'], '</td>
+				<td class="number">', locale_number_format($MyRow['mincharge'], $_SESSION['CompanyRecord']['decimalplaces']), '</td>
+				<td><a href="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8'), '?SelectedShipper=', urlencode($MyRow['shipper_id']), '">', _('Edit'), '</a></td>
+				<td><a href="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8'), '?SelectedShipper=', urlencode($MyRow['shipper_id']), '&amp;delete=1" onclick="return MakeConfirm(\'', _('Are you sure you wish to delete this shipper?'), '\', \'Confirm Delete\', this);">', _('Delete'), '</a></td>
+			</tr>';
 	}
 	//END WHILE LIST LOOP
-	echo '</table>';
+	echo '</tbody>
+		</table>';
 }
 
 if (isset($SelectedShipper)) {
+	echo '<div class="toplink">
+			<a href="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8'), '">', _('Review Records'), '</a>
+		</div>';
+
 	echo '<p class="page_title_text" >
-			<img src="' . $RootPath . '/css/' . $_SESSION['Theme'] . '/images/supplier.png" title="' . _('Search') . '" alt="" />' . ' ' . $Title . '</p>';
-	echo '<div class="centre"><a href="' . htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8') . '">' . _('REVIEW RECORDS') . '</a></div>';
+			<img src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/supplier.png" title="', _('Search'), '" alt="" />', ' ', $Title, '
+		</p>';
 }
 
 if (!isset($_GET['delete'])) {
 
-	echo '<form method="post" action="' . htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8') . '">';
-	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+	echo '<form method="post" action="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8'), '">';
+	echo '<input type="hidden" name="FormID" value="', $_SESSION['FormID'], '" />';
 
 	if (isset($SelectedShipper)) {
 		//editing an existing Shipper
-		$SQL = "SELECT shipper_id, shippername FROM shippers WHERE shipper_id='" . $SelectedShipper . "'";
+		$SQL = "SELECT shipper_id,
+						shippername,
+						mincharge
+					FROM shippers
+					WHERE shipper_id='" . $SelectedShipper . "'";
 
 		$Result = DB_query($SQL);
 		$MyRow = DB_fetch_array($Result);
 
 		$_POST['Shipper_ID'] = $MyRow['shipper_id'];
 		$_POST['ShipperName'] = $MyRow['shippername'];
+		$_POST['MinimumCharge'] = $MyRow['mincharge'];
 
-		echo '<input type="hidden" name="SelectedShipper" value="' . $SelectedShipper . '" />';
-		echo '<input type="hidden" name="Shipper_ID" value="' . $_POST['Shipper_ID'] . '" />';
-		echo '<br /><table>
-						<tr>
-							<td>' . _('Shipper Code') . ':</td>
-							<td>' . $_POST['Shipper_ID'] . '</td>
-						</tr>';
+		echo '<input type="hidden" name="SelectedShipper" value="', $SelectedShipper, '" />';
+		echo '<input type="hidden" name="Shipper_ID" value="', $_POST['Shipper_ID'], '" />';
+		echo '<fieldset>
+				<legend>', _('Edit details for'), ' ', $MyRow['shippername'], '</legend>
+					<field>
+						<label for="Shipper_ID">', _('Shipper Code'), ':</label>
+						<div class="fieldtext">', $_POST['Shipper_ID'], '</div>
+					</field>';
 	} else {
-		echo '<br />
-			<table>';
+		echo '<fieldset>
+				<legend>', _('Create new shipper details'), '</legend>';
 	}
 	if (!isset($_POST['ShipperName'])) {
 		$_POST['ShipperName'] = '';
+		$_POST['MinimumCharge'] = 0;
 	}
 
-	echo '<tr><td>' . _('Shipper Name') . ':</td>
-			<td>
-				<input type="text" name="ShipperName" value="' . $_POST['ShipperName'] . '" size="35" required="required" maxlength="40" />
-			</td>
-		</tr>
+	echo '<field>
+			<label for="ShipperName">', _('Shipper Name'), ':</label>
+			<input type="text" name="ShipperName" value="', $_POST['ShipperName'], '" size="35" required="required" autofocus="autofocus" maxlength="40" />
+			<fieldhelp>', _('Enter a name by which this shipper is known.'), '</fieldhelp>
+		</field>
 
-	</table>
+		<field>
+			<label for="MinimumCharge">', _('Minimum Charge'), ':</label>
+			<input type="text" name="MinimumCharge" class="number" value="', $_POST['MinimumCharge'], '" size="10" required="required" maxlength="10" />
+			<fieldhelp>', _('If this shipper has a minimum charge per shipment, then enter that amount in here.'), '</fieldhelp>
+		</field>
+
+	</fieldset>
 
 	<div class="centre">
-		<input type="submit" name="submit" value="' . _('Enter Information') . '" />
+		<input type="submit" name="submit" value="', _('Enter Information'), '" />
 	</div>
-	</form>';
+</form>';
 
 } //end if record deleted no point displaying form to add record
 include ('includes/footer.php');
