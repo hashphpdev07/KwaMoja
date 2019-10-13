0a0d05c894dd64eadd82ba209d21577cc6c00e8c
Tim Schofield <tim@weberpafrica.com>
2019-10-13 10:18:00 +0100

    Use new css form code
    Files changed in commit:
    ImportSalesPriceList.php
    On branch develop

--- ImportSalesPriceList.php
+++ ImportSalesPriceList.php
@@ -3,7 +3,9 @@ include ('includes/session.php');
 $Title = _('Import Sales Price List');
 include ('includes/header.php');
 
-echo '<p class="page_title_text"><img alt="" src="' . $RootPath . '/css/' . $_SESSION['Theme'] . '/images/maintenance.png" title="' . _('Import Price List from CSV file') . '" />' . ' ' . _('Import Price List from CSV file') . '</p>';
+echo '<p class="page_title_text">
+		<img alt="" src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/maintenance.png" title="', _('Import Price List from CSV file'), '" />', ' ', _('Import Price List from CSV file'), '
+	</p>';
 
 $FieldHeadings = array('StockID', //  0 'STOCKID',
 'SalesType', //  1 'Price list id',
@@ -142,15 +144,28 @@ if (isset($_FILES['PriceListFile']) and $_FILES['PriceListFile']['name']) { //st
 	fclose($FileHandle);
 
 } else { //show file upload form
-	echo '<form action="' . htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8') . '" method="post" enctype="multipart/form-data">';
-	echo '<div class="centre">';
-	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
-	echo '<div class="page_help_text">' . _('This function loads a new sales price list from a comma separated variable (csv) file.') . '<br />' . _('The file must contain four columns, and the first row should be the following headers') . ':<br />StockID, SalesType, CurrencyCode, Price<br />' . _('followed by rows containing these four fields for each price to be uploaded.') . '<br />' . _('The StockID, SalesType, and CurrencyCode fields must have a corresponding entry in the stockmaster, salestypes, and currencies tables.') . '</div>';
+	echo '<form action="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8'), '" method="post" enctype="multipart/form-data">';
+	echo '<input type="hidden" name="FormID" value="', $_SESSION['FormID'], '" />';
 
-	echo '<br /><input type="hidden" name="MAX_FILE_SIZE" value="1000000" />' . _('Prices effective from') . ':&nbsp;<input type="text" name="StartDate" size="10" class="date" value="' . date($_SESSION['DefaultDateFormat']) . '" />&nbsp;' . _('Upload file') . ': <input name="PriceListFile" type="file" />
-			<input type="submit" name="submit" value="' . _('Send File') . '" />
+	echo '<input type="hidden" name="MAX_FILE_SIZE" value="1000000" />';
+
+	echo '<div class="page_help_text">', _('This function loads a new sales price list from a comma separated variable (csv) file.'), '<br />', _('The file must contain four columns, and the first row should be the following headers'), ':<br />StockID, SalesType, CurrencyCode, Price<br />', _('followed by rows containing these four fields for each price to be uploaded.'), '<br />', _('The StockID, SalesType, and CurrencyCode fields must have a corresponding entry in the stockmaster, salestypes, and currencies tables.'), '</div>';
+
+	echo '<fieldset>
+			<legend>', _('Import Criteria'), '</legend>
+			<field>
+				<label for="StartDate">', _('Prices effective from'), '</label>
+				<input type="text" name="StartDate" size="10" class="date" value="', date($_SESSION['DefaultDateFormat']), '" />
+			</field>
+			<field>
+				<label for="PriceListFile">', _('Upload file'), ':</label>
+				<input name="PriceListFile" type="file" />
+			</field>
+		</fieldset>
+		<div class="centre">
+			<input type="submit" name="submit" value="', _('Send File'), '" />
 		</div>
-		</form>';
+	</form>';
 
 }
 
