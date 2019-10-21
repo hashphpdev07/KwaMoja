ace2e0cf1cf329b29f29164df977fd24ca11f2bc
Juergen Mueller <juergenmueller77@yahoo.com>
2019-10-21 15:21:23 +0100

    New calander icon for date input controls omitted from previous commit
    Files changed in commit:
    css/aguapop/images/date.png
    css/arc-dark/images/date.png
    css/default/images/date.png
    css/fluid/images/date.png
    css/fresh/images/date.png
    css/gel/images/date.png
    css/mobile/images/date.png
    css/professional-rtl/images/date.png
    css/professional/images/date.png
    css/silverwolf/images/date.png
    css/wood/images/date.png
    css/xenos/images/date.png
    includes/header.php
    On branch develop

--- includes/header.php
+++ includes/header.php
@@ -24,7 +24,7 @@ echo '<html>
 			<meta http-equiv="Content-Type" content="application/html; charset=utf-8; cache-control: no-cache, no-store, must-revalidate; Pragma: no-cache" />
 			<title>', _('KwaMoja'), ' - ', $Title, '</title>
 			<link rel="icon" href="', $RootPath, '/favicon.ico" />
-			<link href="', $RootPath, '/css/', $_SESSION['Theme'], '/styles.css?v=6" rel="stylesheet" type="text/css" media="screen" />
+			<link href="', $RootPath, '/css/', $_SESSION['Theme'], '/styles.css?v=7" rel="stylesheet" type="text/css" media="screen" />
 			<link href="', $RootPath, '/css/print.css" rel="stylesheet" type="text/css" media="print" />
 			<meta name="viewport" content="width=device-width, initial-scale=1">';
 
