adbab9b44c8bf1f715d6ddf1c189129fd4cc0b9a
Tim Schofield <tim@weberpafrica.com>
2019-10-22 14:17:07 +0100

    Improvements to inquiries
    Files changed in commit:
    OrderDetails.php
    SalesReport.php
    On branch develop

--- OrderDetails.php
+++ OrderDetails.php
@@ -1,21 +1,19 @@
 <?php
-
 /* Session started in header.php for password checking and authorisation level check */
-include('includes/session.php');
+include ('includes/session.php');
 
-$_GET['OrderNumber'] = (int) $_GET['OrderNumber'];
+$_GET['OrderNumber'] = (int)$_GET['OrderNumber'];
 
 if (isset($_GET['OrderNumber'])) {
 	$Title = _('Reviewing Sales Order Number') . ' ' . $_GET['OrderNumber'];
 } else {
-	include('includes/header.php');
-	echo '<br /><br /><br />';
+	include ('includes/header.php');
 	prnMsg(_('This page must be called with a sales order number to review') . '.<br />' . _('i.e.') . ' http://????/OrderDetails.php?OrderNumber=<i>xyz</i><br />' . _('Click on back') . '.', 'error');
-	include('includes/footer.php');
+	include ('includes/footer.php');
 	exit;
 }
 
-include('includes/header.php');
+include ('includes/header.php');
 
 $OrderHeaderSQL = "SELECT salesorders.debtorno,
 							debtorsmaster.name,
@@ -53,11 +51,12 @@ $GetOrdHdrResult = DB_query($OrderHeaderSQL, $ErrMsg, $DbgMsg);
 if (DB_num_rows($GetOrdHdrResult) == 1) {
 
 	echo '<div class="toplink">
-			<a href="' . $RootPath . '/SelectCompletedOrder.php">' . _('Return to Sales Order Inquiry') . '</a>
-			<a href="' . $RootPath . '/SelectCustomer.php">' . _('Return to Customer Inquiry Interface') . '</a>
+			<a href="', $RootPath, '/SelectCompletedOrder.php">', _('Return to Sales Order Inquiry'), '</a><br />
+			<a href="', $RootPath, '/SelectCustomer.php">', _('Return to Customer Inquiry Interface'), '</a>
 		</div>';
+
 	echo '<p class="page_title_text" >
-			<img src="' . $RootPath . '/css/' . $_SESSION['Theme'] . '/images/supplier.png" title="' . _('Order Details') . '" alt="" />' . ' ' . $Title . '
+			<img src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/supplier.png" title="', _('Order Details'), '" alt="" />', ' ', $Title, '
 		</p>';
 
 	$MyRow = DB_fetch_array($GetOrdHdrResult);
@@ -65,7 +64,7 @@ if (DB_num_rows($GetOrdHdrResult) == 1) {
 
 	if ((isset($SupplierLogin) and $SupplierLogin == 0) and $MyRow['debtorno'] != $_SESSION['CustomerID']) {
 		prnMsg(_('Your customer login will only allow you to view your own purchase orders'), 'error');
-		include('includes/footer.php');
+		include ('includes/footer.php');
 		exit;
 	}
 
@@ -77,75 +76,75 @@ if (DB_num_rows($GetOrdHdrResult) == 1) {
 	$InvoiceLink = '';
 	while ($MyInvoiceRow = DB_fetch_array($InvoiceResult)) {
 		if (is_numeric($MyInvoiceRow['transno'])) {
-			$InvoiceLink .= '<a href="' . $RootPath . '/PrintCustTrans.php?FromTransNo=' . $MyInvoiceRow['transno'] . '&InvOrCredit=Invoice">' . $MyInvoiceRow['transno'] . '</a>';
+			$InvoiceLink.= '<a href="' . $RootPath . '/PrintCustTrans.php?FromTransNo=' . $MyInvoiceRow['transno'] . '&InvOrCredit=Invoice">' . $MyInvoiceRow['transno'] . '</a>';
 		}
 	}
 
 	echo '<table>
 			<tr>
-				<th colspan="4"><h3>' . _('Order Header Details For Order No') . ' ' . $_GET['OrderNumber'] . '</h3></th>
+				<th colspan="4"><h3>', _('Order Header Details For Order No'), ' ', $_GET['OrderNumber'], '</h3></th>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Customer Code') . ':</th>
-				<td><a href="' . $RootPath . '/SelectCustomer.php?Select=' . urlencode($MyRow['debtorno']) . '">' . $MyRow['debtorno'] . '</a></td>
-				<th style="text-align: left">' . _('Customer Name') . ':</th>
-				<th>' . $MyRow['name'] . '</th>
+				<th style="text-align: left">', _('Customer Code'), ':</th>
+				<td><a href="', $RootPath, '/SelectCustomer.php?Select=', urlencode($MyRow['debtorno']), '">', $MyRow['debtorno'], '</a></td>
+				<th style="text-align: left">', _('Customer Name'), ':</th>
+				<th>', $MyRow['name'], '</th>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Customer Reference') . ':</th>
-				<td>' . $MyRow['customerref'] . '</td>
-				<th style="text-align: left">' . _('Deliver To') . ':</th>
-				<th>' . $MyRow['deliverto'] . '</th>
+				<th style="text-align: left">', _('Customer Reference'), ':</th>
+				<td>', $MyRow['customerref'], '</td>
+				<th style="text-align: left">', _('Deliver To'), ':</th>
+				<th>', $MyRow['deliverto'], '</th>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Ordered On') . ':</th>
-				<td>' . ConvertSQLDate($MyRow['orddate']) . '</td>
-				<th style="text-align: left">' . _('Delivery Address 1') . ':</th>
-				<td>' . $MyRow['deladd1'] . '</td>
+				<th style="text-align: left">', _('Ordered On'), ':</th>
+				<td>', ConvertSQLDate($MyRow['orddate']), '</td>
+				<th style="text-align: left">', _('Delivery Address 1'), ':</th>
+				<td>', $MyRow['deladd1'], '</td>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Requested Delivery') . ':</th>
-				<td>' . ConvertSQLDate($MyRow['deliverydate']) . '</td>
-				<th style="text-align: left">' . _('Delivery Address 2') . ':</th>
-				<td>' . $MyRow['deladd2'] . '</td>
+				<th style="text-align: left">', _('Requested Delivery'), ':</th>
+				<td>', ConvertSQLDate($MyRow['deliverydate']), '</td>
+				<th style="text-align: left">', _('Delivery Address 2'), ':</th>
+				<td>', $MyRow['deladd2'], '</td>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Order Currency') . ':</th>
-				<td>' . $MyRow['currcode'] . '</td>
-				<th style="text-align: left">' . _('Delivery Address 3') . ':</th>
-				<td">' . $MyRow['deladd3'] . '</td>
+				<th style="text-align: left">', _('Order Currency'), ':</th>
+				<td>', $MyRow['currcode'], '</td>
+				<th style="text-align: left">', _('Delivery Address 3'), ':</th>
+				<td>', $MyRow['deladd3'], '</td>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Deliver From Location') . ':</th>
-				<td>' . $MyRow['fromstkloc'] . '</td>
-				<th style="text-align: left">' . _('Delivery Address 4') . ':</th>
-				<td>' . $MyRow['deladd4'] . '</td>
+				<th style="text-align: left">', _('Deliver From Location'), ':</th>
+				<td>', $MyRow['fromstkloc'], '</td>
+				<th style="text-align: left">', _('Delivery Address 4'), ':</th>
+				<td>', $MyRow['deladd4'], '</td>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Telephone') . ':</th>
-				<td>' . $MyRow['contactphone'] . '</td>
-				<th style="text-align: left">' . _('Delivery Address 5') . ':</th>
-				<td>' . $MyRow['deladd5'] . '</td>
+				<th style="text-align: left">', _('Telephone'), ':</th>
+				<td>', $MyRow['contactphone'], '</td>
+				<th style="text-align: left">', _('Delivery Address 5'), ':</th>
+				<td>', $MyRow['deladd5'], '</td>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Email') . ':</th>
-				<td><a href="mailto:' . $MyRow['contactemail'] . '">' . $MyRow['contactemail'] . '</a></td>
-				<th style="text-align: left">' . _('Delivery Address 6') . ':</th>
-				<td>' . $MyRow['deladd6'] . '</td>
+				<th style="text-align: left">', _('Email'), ':</th>
+				<td><a href="mailto:', $MyRow['contactemail'], '">', $MyRow['contactemail'], '</a></td>
+				<th style="text-align: left">', _('Delivery Address 6'), ':</th>
+				<td>', $MyRow['deladd6'], '</td>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Freight Cost') . ':</th>
-				<td>' . $MyRow['freightcost'] . '</td>
+				<th style="text-align: left">', _('Freight Cost'), ':</th>
+				<td>', $MyRow['freightcost'], '</td>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Comments') . ': </th>
-				<td colspan="3">' . $MyRow['comments'] . '</td>
+				<th style="text-align: left">', _('Comments'), ': </th>
+				<td colspan="3">', $MyRow['comments'], '</td>
 			</tr>
 			<tr>
-				<th style="text-align: left">' . _('Invoices') . ': </th>
-				<td colspan="3">' . $InvoiceLink . '</td>
+				<th style="text-align: left">', _('Invoices'), ': </th>
+				<td colspan="3">', $InvoiceLink, '</td>
  			</tr>
-			</table>';
+		</table>';
 }
 
 /*Now get the line items */
@@ -185,20 +184,20 @@ if (DB_num_rows($LineItemsResult) > 0) {
 
 	echo '<table>
 			<tr>
-				<th colspan="9"><h3>' . _('Order Line Details For Order No') . ' ' . $_GET['OrderNumber'] . '</h3></th>
+				<th colspan="11"><h3>', _('Order Line Details For Order No'), ' ', $_GET['OrderNumber'], '</h3></th>
 			</tr>
 			<tr>
-				<th>' . _('PO Line') . '</th>
-				<th>' . _('Item Code') . '</th>
-				<th>' . _('Item Description') . '</th>
-				<th>' . _('Quantity') . '</th>
-				<th>' . _('Unit') . '</th>
-				<th>' . _('Price') . '</th>
-				<th>' . _('Discount') . '</th>
-				<th>' . _('Total') . '</th>
-				<th>' . _('Qty Del') . '</th>
-				<th>' . _('Last Del') . '/' . _('Due Date') . '</th>
-				<th>' . _('Narrative') . '</th>
+				<th>', _('PO Line'), '</th>
+				<th>', _('Item Code'), '</th>
+				<th>', _('Item Description'), '</th>
+				<th>', _('Quantity'), '</th>
+				<th>', _('Unit'), '</th>
+				<th>', _('Price'), '</th>
+				<th>', _('Discount'), '</th>
+				<th>', _('Total'), '</th>
+				<th>', _('Qty Del'), '</th>
+				<th>', _('Last Del'), '/', _('Due Date'), '</th>
+				<th>', _('Narrative'), '</th>
 			</tr>';
 	$k = 0;
 	while ($MyRow = DB_fetch_array($LineItemsResult)) {
@@ -206,48 +205,48 @@ if (DB_num_rows($LineItemsResult) > 0) {
 		if ($MyRow['qtyinvoiced'] > 0) {
 			$DisplayActualDeliveryDate = ConvertSQLDate($MyRow['actualdispatchdate']);
 		} else {
-	  		$DisplayActualDeliveryDate = '<span style="color:red;">' . ConvertSQLDate($MyRow['itemdue']) . '</span>';
+			$DisplayActualDeliveryDate = '<span style="color:red;">' . ConvertSQLDate($MyRow['itemdue']) . '</span>';
 		}
 
 		echo '<tr class="striped_row">
-				<td>' . $MyRow['poline'] . '</td>
-				<td>' . $MyRow['stkcode'] . '</td>
-				<td>' . $MyRow['description'] . '</td>
-				<td class="number">' . $MyRow['quantity'] . '</td>
-				<td>' . $MyRow['units'] . '</td>
-				<td class="number">' . locale_number_format($MyRow['unitprice'], $CurrDecimalPlaces) . '</td>
-				<td class="number">' . locale_number_format(($MyRow['discountpercent'] * 100), 2) . '%' . '</td>
-				<td class="number">' . locale_number_format($MyRow['quantity'] * $MyRow['unitprice'] * (1 - $MyRow['discountpercent']), $CurrDecimalPlaces) . '</td>
-				<td class="number">' . locale_number_format($MyRow['qtyinvoiced'], $MyRow['decimalplaces']) . '</td>
-				<td>' . $DisplayActualDeliveryDate . '</td>
-				<td>' . $MyRow['narrative'] . '</td>
+				<td>', $MyRow['poline'], '</td>
+				<td>', $MyRow['stkcode'], '</td>
+				<td>', $MyRow['description'], '</td>
+				<td class="number">', $MyRow['quantity'], '</td>
+				<td>', $MyRow['units'], '</td>
+				<td class="number">', locale_number_format($MyRow['unitprice'], $CurrDecimalPlaces), '</td>
+				<td class="number">', locale_number_format(($MyRow['discountpercent'] * 100), 2), '%', '</td>
+				<td class="number">', locale_number_format($MyRow['quantity'] * $MyRow['unitprice'] * (1 - $MyRow['discountpercent']), $CurrDecimalPlaces), '</td>
+				<td class="number">', locale_number_format($MyRow['qtyinvoiced'], $MyRow['decimalplaces']), '</td>
+				<td>', $DisplayActualDeliveryDate, '</td>
+				<td>', $MyRow['narrative'], '</td>
 			</tr>';
 
-		$OrderTotal += ($MyRow['quantity'] * $MyRow['unitprice'] * (1 - $MyRow['discountpercent']));
-		$OrderTotalVolume += ($MyRow['quantity'] * $MyRow['volume']);
-		$OrderTotalWeight += ($MyRow['quantity'] * $MyRow['grossweight']);
+		$OrderTotal+= ($MyRow['quantity'] * $MyRow['unitprice'] * (1 - $MyRow['discountpercent']));
+		$OrderTotalVolume+= ($MyRow['quantity'] * $MyRow['volume']);
+		$OrderTotalWeight+= ($MyRow['quantity'] * $MyRow['grossweight']);
 
 	}
 	$DisplayTotal = locale_number_format($OrderTotal, $CurrDecimalPlaces);
 	$DisplayVolume = locale_number_format($OrderTotalVolume, 2);
 	$DisplayWeight = locale_number_format($OrderTotalWeight, 2);
 
-	echo '<tr>
-				<td colspan="5" class="number"><b>' . _('TOTAL Excl Tax/Freight') . '</b></td>
-				<td colspan="2" class="number">' . $DisplayTotal . '</td>
-			</tr>
-			</table>';
+	echo '<tr class="total_row">
+			<td colspan="6" class="number"><b>', _('TOTAL Excl Tax/Freight'), '</b></td>
+			<td colspan="2" class="number">', $DisplayTotal, '</td>
+			<td colspan="3"></td>
+		</tr>
+	</table>';
 
-	echo '<br />
-			<table>
+	echo '<table>
 			<tr>
-				<td>' . _('Total Weight') . ':</td>
-				<td>' . $DisplayWeight . '</td>
-				<td>' . _('Total Volume') . ':</td>
-				<td>' . $DisplayVolume . '</td>
+				<td>', _('Total Weight'), ':</td>
+				<td>', $DisplayWeight, '</td>
+				<td>', _('Total Volume'), ':</td>
+				<td>', $DisplayVolume, '</td>
 			</tr>
 		</table>';
 }
 
-include('includes/footer.php');
+include ('includes/footer.php');
 ?>
