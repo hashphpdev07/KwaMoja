649a37a3cf32d3463e044b2aecdcc271d84c9fb2
Tim Schofield <tim@weberpafrica.com>
2019-11-11 15:04:31 +0000

    Use new forms.css Neaten up table with proper heading and striped rows
    Files changed in commit:
    StockSerialItemResearch.php
    On branch develop

--- StockSerialItemResearch.php
+++ StockSerialItemResearch.php
@@ -6,8 +6,8 @@ $Title = _('Serial Item Research');
 include ('includes/header.php');
 
 echo '<p class="page_title_text" >
-		<img src="' . $RootPath . '/css/' . $_SESSION['Theme'] . '/images/inventory.png" title="' . _('Inventory') . '" alt="" /><b>' . $Title . '</b>
-	  </p>';
+		<img src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/inventory.png" title="', _('Inventory'), '" alt="" /><b>', $Title, '</b>
+	</p>';
 
 //validate the submission
 if (isset($_POST['serialno'])) {
@@ -18,11 +18,22 @@ if (isset($_POST['serialno'])) {
 	$SerialNo = '';
 }
 
-echo '<form id="SerialNoResearch" method="post" action="' . htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8') . '">';
-echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+echo '<form id="SerialNoResearch" method="post" action="', htmlspecialchars(basename(__FILE__), ENT_QUOTES, 'UTF-8'), '">';
+echo '<input type="hidden" name="FormID" value="', $_SESSION['FormID'], '" />';
 
-echo _('Serial Number') . ': <input id="serialno" type="text" name="serialno" size="21" required="required" maxlength="20" value="' . $SerialNo . '" /> &nbsp;<input type="submit" name="submit" value="' . _('Search') . '" />
-</form><br />';
+echo '<fieldset>
+		<legend>', _('Inquiry Criteria'), '</legend>
+		<field>
+			<label for="serialno">', _('Serial Number'), ':</label>
+			<input id="serialno" type="text" name="serialno" size="21" required="required" maxlength="20" value="', $SerialNo, '" />
+		</field>
+	</fieldset>';
+
+echo '<div class="centre">
+		<input type="submit" name="submit" value="', _('Search'), '" />
+	</div>';
+
+echo '</form>';
 
 if ($SerialNo != '') {
 	//the point here is to allow a semi fuzzy search, but still keep someone from killing the db server
@@ -71,35 +82,37 @@ if ($SerialNo != '') {
 	if (DB_num_rows($Result) == 0) {
 		prnMsg(_('No History found for Serial Number') . ': <b>' . $SerialNo . '</b>', 'warn');
 	} else {
-		echo '<h4>' . _('Details for Serial Item') . ': <b>' . $SerialNo . '</b><br />' . _('Length') . '=' . mb_strlen($SerialNo) . '</h4>';
 		echo '<table>';
 		echo '<tr>
-				<th>' . _('StockID') . '</th>
-				<th>' . _('CurInvQty') . '</th>
-				<th>' . _('Move Qty') . '</th>
-				<th>' . _('Move Type') . '</th>
-				<th>' . _('Trans #') . '</th>
-				<th>' . _('Location') . '</th>
-				<th>' . _('Date') . '</th>
-				<th>' . _('DebtorNo') . '</th>
-				<th>' . _('Branch') . '</th>
-				<th>' . _('Move Ref') . '</th>
-				<th>' . _('Total Move Qty') . '</th>
+				<th colspan="11"><h4>', _('Details for Serial Item'), ': <b>', $SerialNo, '</b><br />', _('Length'), '=', mb_strlen($SerialNo), '</h4></th>
+			</tr>';
+		echo '<tr>
+				<th>', _('StockID'), '</th>
+				<th>', _('CurInvQty'), '</th>
+				<th>', _('Move Qty'), '</th>
+				<th>', _('Move Type'), '</th>
+				<th>', _('Trans #'), '</th>
+				<th>', _('Location'), '</th>
+				<th>', _('Date'), '</th>
+				<th>', _('DebtorNo'), '</th>
+				<th>', _('Branch'), '</th>
+				<th>', _('Move Ref'), '</th>
+				<th>', _('Total Move Qty'), '</th>
 			</tr>';
 		while ($MyRow = DB_fetch_row($Result)) {
-			printf('<tr>
-					<td>%s<br />%s</td>
-					<td class="number">%s</td>
-					<td class="number">%s</td>
-					<td>%s (%s)</td>
-					<td class="number">%s</td>
-					<td>%s - %s</td>
-					<td>%s &nbsp;</td>
-					<td>%s &nbsp;</td>
-					<td>%s &nbsp;</td>
-					<td>%s &nbsp;</td>
-					<td class="number">%s</td>
-					</tr>', $MyRow[1], $MyRow[0], $MyRow[2], $MyRow[3], $MyRow[5], $MyRow[4], $MyRow[6], $MyRow[7], $MyRow[8], $MyRow[9], $MyRow[10], $MyRow[11], $MyRow[12], $MyRow[13]);
+			echo '<tr class="striped_row">
+					<td>', $MyRow[1], '<br />', $MyRow[0], '</td>
+					<td class="number">', $MyRow[2], '</td>
+					<td class="number">', $MyRow[3], '</td>
+					<td>', $MyRow[5], ' (', $MyRow[4], ')</td>
+					<td class="number">', $MyRow[6], '</td>
+					<td>', $MyRow[7], ' - ', $MyRow[8], '</td>
+					<td>', $MyRow[9], ' &nbsp;</td>
+					<td>', $MyRow[10], ' &nbsp;</td>
+					<td>', $MyRow[11], ' &nbsp;</td>
+					<td>', $MyRow[12], ' &nbsp;</td>
+					<td class="number">', $MyRow[13], '</td>
+				</tr>';
 		} //END WHILE LIST LOOP
 		echo '</table>';
 	} // ELSE THERE WHERE ROWS
