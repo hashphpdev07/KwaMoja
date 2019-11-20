b6072b425700d40a0a35afc6eebc4c607e54f331
Tim Schofield <tim@weberpafrica.com>
2019-11-20 11:25:18 +0000

    Fix headings for consistency with other scripts Initialise the counter variable Change redundant printf statements to echo Use correct css class for total row
    Files changed in commit:
    MaterialsNotUsed.php
    On branch develop

--- MaterialsNotUsed.php
+++ MaterialsNotUsed.php
@@ -16,7 +16,7 @@ $SQL = "SELECT stockmaster.stockid,
 				WHERE locstock.stockid = stockmaster.stockid) AS qoh
 		FROM stockmaster
 		LEFT JOIN stockcosts
-			ON stockcosts.stockid=stockmater.stockid
+			ON stockcosts.stockid=stockmaster.stockid
 			AND stockcosts.succeeded=0
 		INNER JOIN stockcategory
 			ON stockmaster.categoryid = stockcategory.categoryid
@@ -28,40 +28,44 @@ $SQL = "SELECT stockmaster.stockid,
 				WHERE bom.component = stockmaster.stockid )
 		ORDER BY stockmaster.stockid";
 $Result = DB_query($SQL);
-echo '<p class="page_title_text" align="center"><strong>' . _('Raw Materials Not Used in any BOM') . '</strong></p>';
+
+echo '<p class="page_title_text">
+		<img src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/inventory.png" title="', _('Search'), '" alt="" />', ' ', _('Raw Materials Not Used in any BOM'), '
+	</p>';
+
 if (DB_num_rows($Result) != 0) {
 	$TotalValue = 0;
 	echo '<table>
 			<tr>
-				<th>' . _('#') . '</th>
-				<th>' . _('Code') . '</th>
-				<th>' . _('Description') . '</th>
-				<th>' . _('QOH') . '</th>
-				<th>' . _('Std Cost') . '</th>
-				<th>' . _('Value') . '</th>
+				<th>', _('#'), '</th>
+				<th>', _('Code'), '</th>
+				<th>', _('Description'), '</th>
+				<th>', _('QOH'), '</th>
+				<th>', _('Std Cost'), '</th>
+				<th>', _('Value'), '</th>
 			</tr>';
-
+	$i = 0;
 	while ($MyRow = DB_fetch_array($Result)) {
 		$CodeLink = '<a href="' . $RootPath . '/SelectProduct.php?StockID=' . urlencode($MyRow['stockid']) . '">' . $MyRow['stockid'] . '</a>';
 		$LineValue = $MyRow['qoh'] * $MyRow['stdcost'];
 		$TotalValue = $TotalValue + $LineValue;
 
-		printf('<tr class="striped_row">
-					<td class="number">%s</td>
-					<td>%s</td>
-					<td>%s</td>
-					<td class="number">%s</td>
-					<td class="number">%s</td>
-					<td class="number">%s</td>
-				</tr>', $i, $CodeLink, $MyRow['description'], locale_number_format($MyRow['qoh'], $MyRow['decimalplaces']), locale_number_format($MyRow['stdcost'], $_SESSION['CompanyRecord']['decimalplaces']), locale_number_format($LineValue, $_SESSION['CompanyRecord']['decimalplaces']));
+		echo '<tr class="striped_row">
+				<td class="number">', $i, '</td>
+				<td>', $CodeLink, '</td>
+				<td>', $MyRow['description'], '</td>
+				<td class="number">', locale_number_format($MyRow['qoh'], $MyRow['decimalplaces']), '</td>
+				<td class="number">', locale_number_format($MyRow['stdcost'], $_SESSION['CompanyRecord']['decimalplaces']), '</td>
+				<td class="number">', locale_number_format($LineValue, $_SESSION['CompanyRecord']['decimalplaces']), '</td>
+			</tr>';
 		++$i;
 	}
 
-	printf('<tr class="striped_row">
-				<td colspan="4">%s</td>
-				<td>%s</td>
-				<td class="number">%s</td>
-			</tr>', '', _('Total') . ':', locale_number_format($TotalValue, $_SESSION['CompanyRecord']['decimalplaces']));
+	echo '<tr class="total_row">
+			<td colspan="4"></td>
+			<td>', _('Total'), ':</td>
+			<td class="number">', locale_number_format($TotalValue, $_SESSION['CompanyRecord']['decimalplaces']), '</td>
+		</tr>';
 
 	echo '</table>';
 } else {
