e7d5de79e7aacd719a5633cc1f32ba425c928b0f
Tim Schofield <tim@weberpafrica.com>
2020-08-19 10:05:31 +0100

    New pathology lab scripts
    Files changed in commit:
    KCMCMedicalLaboratory.php
    KCMCPathologyLaboratory.php
    KCMCPendingPathologyTests.php
    KCMCRequestPathologyTest.php
    KCMCViewPathologyTest.php
    sql/updates/218.php
    On branch develop

--- KCMCPathologyLaboratory.php
+++ KCMCPathologyLaboratory.php
@@ -2,25 +2,25 @@
 /* Selection of patient - from where all patient related maintenance, transactions and inquiries start */
 
 include ('includes/session.php');
-$Title = _('Medical Laboratory');
+$Title = _('Pathology Laboratory');
 $ViewTopic = '';
 $BookMark = '';
 include ('includes/header.php');
 
 echo '<p class="page_title_text">
-		<img alt="" src="', $RootPath, '/css/', $_SESSION['Theme'], '/images-medical/labtest.png" title="', _('Medical Laboratory'), '" /> ', _('Medical Laboratory'), '
+		<img alt="" src="', $RootPath, '/css/', $_SESSION['Theme'], '/images-medical/labtest.png" title="', _('Pathology Laboratory'), '" /> ', _('Pathology Laboratory'), '
 	</p>';
 
 include ('includes/SQL_CommonFunctions.php');
 include ('includes/HospitalFunctions.php');
 
 echo '<fieldset style="text-align:center">';
-// Customer inquiries options:
+
 echo '<fieldset class="MenuList">
 		<legend><img alt="" src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/reports.png" data-title="', _('Inquiries and Reports'), '" />', _('Lobaoratory Inquiries'), '</legend>
 		<ul>
 			<li class="MenuItem">
-				<a href="', $RootPath, '/KCMCPendingLaboratoryTests.php">', _('Pending Laboratory Tests'), '</a>
+				<a href="', $RootPath, '/KCMCPendingPathologyTests.php">', _('Pending Pathology Tests'), '</a>
 			</li>
 		</ul>
 	</fieldset>';
@@ -29,7 +29,7 @@ echo '<fieldset class="MenuList">
 		<legend><img alt="" src="', $RootPath, '/css/', $_SESSION['Theme'], '/images/transactions.png" data-title="', _('Lobaoratory Transactions'), '" />', _('Lobaoratory Transactions'), '</legend>
 		<ul>
 			<li class="MenuItem">
-				<a href="', $RootPath, '/KCMCRequestLaboratoryTest.php?New=Yes">', _('Request a Laboratory Test'), '</a>
+				<a href="', $RootPath, '/KCMCRequestPathologyTest.php?New=Yes">', _('Request a Pathology Test'), '</a>
 			</li>
 		</ul>
 	</fieldset>';
--- sql/updates/218.php
+++ sql/updates/218.php
@@ -2,9 +2,18 @@
 NewScript('KCMCRequestLaboratoryTest.php', 13);
 NewScript('KCMCPendingLaboratoryTests.php', 13);
 NewScript('KCMCViewLaboratoryTest.php', 13);
-NewScript('KCMCMedicalLaboratory.php', 13);
 NewScript('KCMCEnterLaboratoryTestResults.php', 13);
+
+NewScript('KCMCRequestPathologyTest.php', 13);
+NewScript('KCMCPendingPathologyTests.php', 13);
+NewScript('KCMCViewPathologyTest.php', 13);
+NewScript('KCMCEnterPathologyTestResults.php', 13);
+
+NewScript('KCMCMedicalLaboratory.php', 13);
 NewMenuItem('hospital', 'Reports', _('Medical Laboratory Details'), '/KCMCMedicalLaboratory.php', 3);
+
+NewScript('KCMCPathologyLaboratory.php', 13);
+NewMenuItem('hospital', 'Reports', _('Pathology Laboratory Details'), '/KCMCPathologyLaboratory.php', 4);
 
 CreateTable('care_test_request_chemlabor', "CREATE TABLE IF NOT EXISTS `care_test_request_chemlabor` (
   `batch_nr` int(11) NOT NULL auto_increment,
@@ -103,6 +112,76 @@ CreateTable('care_test_findings_chemlabor_sub', "CREATE TABLE IF NOT EXISTS `car
   `create_id` varchar(35) character set latin1 collate latin1_general_ci default NULL,
   `create_time` timestamp NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (`sub_id`)
+)");
+
+CreateTable('care_test_request_patho', "CREATE TABLE IF NOT EXISTS `care_test_request_patho` (
+  `batch_nr` int(11) unsigned NOT NULL auto_increment,
+  `encounter_nr` int(11) unsigned NOT NULL default '0',
+  `dept_nr` smallint(5) unsigned NOT NULL default '0',
+  `quick_cut` tinyint(4) NOT NULL default '0',
+  `qc_phone` varchar(40) collate latin1_general_ci NOT NULL,
+  `quick_diagnosis` tinyint(4) NOT NULL default '0',
+  `qd_phone` varchar(40) collate latin1_general_ci NOT NULL,
+  `material_type` varchar(25) collate latin1_general_ci NOT NULL,
+  `material_desc` text collate latin1_general_ci NOT NULL,
+  `localization` tinytext collate latin1_general_ci NOT NULL,
+  `clinical_note` tinytext collate latin1_general_ci NOT NULL,
+  `extra_note` tinytext collate latin1_general_ci NOT NULL,
+  `repeat_note` tinytext collate latin1_general_ci NOT NULL,
+  `gyn_last_period` varchar(25) collate latin1_general_ci NOT NULL,
+  `gyn_period_type` varchar(25) collate latin1_general_ci NOT NULL,
+  `gyn_gravida` varchar(25) collate latin1_general_ci NOT NULL,
+  `gyn_menopause_since` varchar(25) collate latin1_general_ci NOT NULL default '0',
+  `gyn_hysterectomy` varchar(25) collate latin1_general_ci NOT NULL default '0',
+  `gyn_contraceptive` varchar(25) collate latin1_general_ci NOT NULL default '0',
+  `gyn_iud` varchar(25) collate latin1_general_ci NOT NULL,
+  `gyn_hormone_therapy` varchar(25) collate latin1_general_ci NOT NULL,
+  `doctor_sign` varchar(35) collate latin1_general_ci NOT NULL,
+  `op_date` date NOT NULL default '0000-00-00',
+  `send_date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `status` varchar(10) collate latin1_general_ci NOT NULL,
+  `entry_date` date NOT NULL default '0000-00-00',
+  `journal_nr` varchar(15) collate latin1_general_ci NOT NULL,
+  `blocks_nr` int(11) NOT NULL default '0',
+  `deep_cuts` int(11) NOT NULL default '0',
+  `special_dye` varchar(35) collate latin1_general_ci NOT NULL,
+  `immune_histochem` varchar(35) collate latin1_general_ci NOT NULL,
+  `hormone_receptors` varchar(35) collate latin1_general_ci NOT NULL,
+  `specials` varchar(35) collate latin1_general_ci NOT NULL,
+  `history` text collate latin1_general_ci,
+  `modify_id` varchar(35) collate latin1_general_ci NOT NULL,
+  `modify_time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `create_id` varchar(35) collate latin1_general_ci NOT NULL,
+  `create_time` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `process_id` varchar(35) collate latin1_general_ci NOT NULL,
+  `process_time` timestamp NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY  (`batch_nr`),
+  KEY `encounter_nr` (`encounter_nr`),
+  KEY `send_date` (`send_date`)
+)");
+
+CreateTable('care_test_findings_patho', "CREATE TABLE IF NOT EXISTS `care_test_findings_patho` (
+  `batch_nr` int(11) NOT NULL default '0',
+  `encounter_nr` int(11) unsigned NOT NULL default '0',
+  `room_nr` varchar(10) collate latin1_general_ci NOT NULL,
+  `dept_nr` smallint(5) unsigned NOT NULL default '0',
+  `material` text collate latin1_general_ci NOT NULL,
+  `macro` text collate latin1_general_ci NOT NULL,
+  `micro` text collate latin1_general_ci NOT NULL,
+  `findings` text collate latin1_general_ci NOT NULL,
+  `diagnosis` text collate latin1_general_ci NOT NULL,
+  `doctor_id` varchar(35) collate latin1_general_ci NOT NULL,
+  `findings_date` date NOT NULL default '0000-00-00',
+  `findings_time` time NOT NULL default '00:00:00',
+  `status` varchar(10) collate latin1_general_ci NOT NULL,
+  `history` text collate latin1_general_ci NOT NULL,
+  `modify_id` varchar(35) collate latin1_general_ci NOT NULL,
+  `modify_time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `create_id` varchar(35) collate latin1_general_ci NOT NULL,
+  `create_time` timestamp NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY  (`batch_nr`,`encounter_nr`,`room_nr`,`dept_nr`),
+  KEY `send_date` (`findings_date`),
+  KEY `findings_date` (`findings_date`)
 )");
 
 UpdateDBNo(basename(__FILE__, '.php'));
