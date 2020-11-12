d24a4cdbbc8ba238043f3c5435829f5ad9a09db9
Tim Schofield <tim@weberpafrica.com>
2020-11-12 09:59:46 +0000

    Remove the default for the description field - Not allowed by some sql modes
    Files changed in commit:
    sql/updates/222.php
    On branch develop

--- sql/updates/222.php
+++ sql/updates/222.php
@@ -8,7 +8,7 @@ CreateTable('glbudgetheaders', "CREATE TABLE IF NOT EXISTS `glbudgetheaders` (
   `id` int(11) NOT NULL auto_increment,
   `owner` varchar(20) NOT NULL DEFAULT '',
   `name` varchar(200) NOT NULL DEFAULT '',
-  `description` text NOT NULL DEFAULT '',
+  `description` text,
   `startperiod` smallint(6) NOT NULL DEFAULT 0,
   `endperiod` smallint(6) NOT NULL DEFAULT 0,
   `current` tinyint(1) NOT NULL DEFAULT 0,
