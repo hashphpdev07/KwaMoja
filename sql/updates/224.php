ea7b98f8752f1d4e26040eede8b2cf7cec2f18ce
Tim Schofield <tim@weberpafrica.com>
2020-11-19 23:21:54 +0000

    Correctly create new gltotals table content from gltrans table
    Files changed in commit:
    sql/updates/224.php
    On branch develop

--- sql/updates/224.php
+++ sql/updates/224.php
@@ -8,9 +8,25 @@ CreateTable('gltotals', "CREATE TABLE IF NOT EXISTS `gltotals` (
 
 $SQL = "TRUNCATE gltotals";
 $Result = DB_query($SQL);
-$SQL = "INSERT INTO gltotals (account, period, amount)
-		SELECT account, periodno, SUM(amount) as total FROM gltrans GROUP BY account,periodno";
-$Result = DB_query($SQL);
+
+$PeriodsSQL = "SELECT periodno FROM periods";
+$PeriodsResult = DB_query($PeriodSQL);
+while ($PeriodRow = DB_fetch_array($PeriodsResult)) {
+	$CreateEntriesSQL = "INSERT INTO gltotals (account, period, amount) SELECT accountcode, '" . $PeriodRow['periodno'] . "', 0 FROM chartmaster";
+	$CreateEntriesResult = DB_query($CreateEntriesSQL);
+}
+
+$TotalsSQL = "SELECT account, period FROM gltotals";
+$TotalsResult = DB_query($TotalsSQL);
+while ($TotalsRow = DB_fetch_array($TotalsResult)) {
+	$TotalSum = "SELECT SUM(amount) as total FROM gltrans WHERE account='" . $TotalsRow['account'] . "' AND periodno='" . $TotalsRow['periodno'] . "'";
+	$TotalResult = DB_query($TotalSum);
+	$TotalRow = DB_fetch_array($TotalResult);
+	$UpdateSQL = "UPDATE gltotals SET amount='" . $TotalRow['total'] . "'
+									WHERE account='" . $TotalsRow['account'] . "'
+									AND periodno='" . $TotalsRow['periodno'] . "'";
+	$UpdateResult = DB_query($UpdateSQL);
+}
 
 UpdateDBNo(basename(__FILE__, '.php'));
 
